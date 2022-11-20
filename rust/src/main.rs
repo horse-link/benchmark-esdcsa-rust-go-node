f85d0288b5a6519c9625480b88672ef939a99016
Lorenzo Fontoura <lorenzo@nioel.com>
2022-11-20 12:31:59 +1000

    refactor rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,38 +8,28 @@ use std::thread;
 use ethers::types::{Address, Signature};
 use ethers::types::transaction::eip1559::Eip1559TransactionRequest;
 use ethers::types::transaction::eip2718::TypedTransaction;
+use rayon::prelude::*;
 
-const REPEAT_COUNT: u32 = 100_000;
-const THREAD_COUNT: u32 = 100;
+const REPEAT_COUNT: usize = 100_000;
 
 fn main() {
     let start = SystemTime::now();
-    let mut handles = vec![];
-    let hashes_per_thread = REPEAT_COUNT / THREAD_COUNT;
-    for _ in 0..THREAD_COUNT {
-        let handle = thread::spawn(move || {
-            for _ in 0..hashes_per_thread {
-                hash();
-            }
-        });
-        handles.push(handle);
-    }
-    for handle in handles {
-        handle.join().unwrap();
-    }
+    let signer = get_signer();
+    let _: Vec<Signature>  = vec![0; REPEAT_COUNT]
+        .par_iter()
+        .map(|_| hash(&signer))
+        .collect();
     let duration = SystemTime::now().duration_since(start).unwrap();
-    println!("execution took {:#?} milliseconds", duration.as_millis());
+    println!("{:#?}", duration.as_millis());
 }
 
-fn hash() -> Signature {
+fn hash(signer: &LocalWallet) -> Signature {
     // Hash message.
     let hash = ethers::utils::keccak256(ethers::abi::encode(&[
         Token::Uint(U256::from(1337)),
         Token::String("The quick brown fox jumps over the lazy dog".to_string()),
     ]));
     // Sign EIP191 message.
-    let signer = get_signer();
-    // Signs the message.
     let signature = signer.sign_hash(ethers::utils::hash_message(hash));
     // Build the tx.
     let address = Address::from_str("0xeC9D1C79A92A6c108b6aa9B101E86d58034843B5").unwrap();
